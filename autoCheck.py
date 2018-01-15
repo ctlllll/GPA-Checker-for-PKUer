dee33e36188b9579b0e776a32a0feb26fd66d927
Tianle Cai <caitianle1998@pku.edu.cn>
2018-01-16 02:32:59 +0800

    Debug

--- autoCheck.py
+++ autoCheck.py
@@ -8,6 +8,7 @@ while True:
     userData = pickle.load(open('userData', 'rb+'))
     mailData = pickle.load(open('mailData', 'rb+'))
     for userName in userData.keys():
+        sess.__init__()
         print(userName, userData[userName])
         if userName in output.keys():
             last = output[userName]
