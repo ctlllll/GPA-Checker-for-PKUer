cb131871844dec7f816672c2b1d0cf89ee02154c
Tianle Cai <caitianle1998@pku.edu.cn>
2018-01-16 01:12:06 +0800

    Autocheck for server

---
+++ autoCheck.py
@@ -0,0 +1,27 @@
+from time import sleep
+import pickle
+from GPAChecker import portal
+
+sess = portal()
+output = {}
+while True:
+    userData = pickle.load(open('userData', 'rb+'))
+    mailData = pickle.load(open('mailData', 'rb+'))
+    for userName in userData.keys():
+        print(userName, userData[userName])
+        if userName in output:
+            last = output[userName]
+            sess.userName = userName
+            sess.password = userData[userName]
+            sess.login()
+            new = sess.getOutput()
+            if last != new:
+                sess.sendMailto(new, mailData[userName])
+            sleep(1)
+        else:
+            sess.userName = userName
+            sess.password = userData[userName]
+            sess.login()
+            output[userName] = sess.getOutput()
+            sleep(1)
+    sleep(3)
